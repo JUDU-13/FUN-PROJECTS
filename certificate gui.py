8235e97d87ede8c3a32cf2068dbfe649147d9a49
JAIDON GILL SHAJAN <84973988+JUDU-13@users.noreply.github.com>
2023-02-06 20:02:50 +0530

    add

---
+++ certificate gui.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+
+def print_certificate():
+    # Get the values from the GUI
+    name = name_entry.get()
+    course = course_entry.get()
+
+    # Print the certificate
+    print(f"Certificate of Completion\n\nThis is to certify that {name}\nhas successfully completed the course\n{course}")
+
+# Create the main window
+root = tk.Tk()
+root.title("Certificate Printer")
+
+# Create the name label and entry
+name_label = tk.Label(root, text="Name:")
+name_entry = tk.Entry(root)
+
+# Create the course label and entry
+course_label = tk.Label(root, text="Course:")
+course_entry = tk.Entry(root)
+
+# Create the print button
+print_button = tk.Button(root, text="Print", command=print_certificate)
+
+# Pack the GUI elements
+name_label.pack()
+name_entry.pack()
+course_label.pack()
+course_entry.pack()
+print_button.pack()
+
+# Start the main event loop
+root.mainloop()
